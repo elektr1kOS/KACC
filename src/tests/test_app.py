86ee94c833329b276abc1e6e5518cdfd7671885f
Kenzo <totallykenzo@gmail.com>
2024-04-21 12:45:46 +1000

    Fixed tests

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -4,8 +4,10 @@ from srctools import bsp
 
 
 def test_app():
-    assert "models/props/portal_door_combined_new.mdl" in bsp.BSP(
-        os.path.join(os.getcwd(), "test_packmap.bsp")).pakfile.namelist()
+    for file in os.listdir(os.getcwd()):
+        if file.endswith("test_packmap.bsp"):
+            assert "models/props/portal_door_combined_new.mdl" in bsp.BSP(
+                os.path.join(os.getcwd(), "test_packmap.bsp")).pakfile.namelist()
 
 
 test_app()
