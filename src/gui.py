41c48b86ae65bbaf3db8dac7ee36dcabc1470783
electrovoyage <75689188+TPEcool@users.noreply.github.com>
2024-04-21 10:57:17 +0300

    Create bare bones of GUI and add imports to GUI version

---
+++ src/gui.py
@@ -0,0 +1,14 @@
+'''
+GUI version of KACC.
+'''
+
+from srctools import bsp
+from tkinter.filedialog import askopenfile, asksaveasfile
+from ttkbootstrap import *
+import json
+
+win = Window('KACC (GUI version)', 'darkly')
+
+
+
+win.mainloop()
